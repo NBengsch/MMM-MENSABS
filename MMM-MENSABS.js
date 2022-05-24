dae82e19c02f2345238fb6f1a9705522fcab193f
NBengsch <niklas.bengsch@bvb.lavie-reha.de>
2022-05-24 10:12:17 +0200

    change

---
+++ MMM-MENSABS.js
@@ -0,0 +1,12 @@
+Module.register("MMM-MENSABS",{
+    defaults: {
+		text: "Hello World!"
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = this.config.text;
+		return wrapper;
+	}
+})
